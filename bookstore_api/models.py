fa6d4afafb0300a96b3e8275b21c815c21a85aca
MarkWengSTR <bskin0330@gmail.com>
2021-04-20 13:55:27 +0800

    Initial model and test data import by example

--- bookstore_api/models.py
+++ bookstore_api/models.py
@@ -1,5 +1,32 @@
 from django.db import models
 
-# Create your models here.
-class Music(models.Model):
+# # Create your models here.
 
+
+class BookStore(models.Model):
+    cash_balance = models.FloatField(null=False)
+    store_name = models.CharField(max_length=100, null=False)
+
+
+class Book(models.Model):
+    books_store = models.ForeignKey(BookStore, on_delete=models.CASCADE)
+    book_name = models.CharField(max_length=100, null=False)
+    price = models.FloatField(null=False)
+
+
+class OpeningHour(models.Model):
+    class WeekDay(models.TextChoices):
+        MON = "Mon", "Monday"
+        TUES = "Tues", "Tuesday"
+        WED = "Wed", "Wednesday"
+        THURS = "Thurs", "Thursday"
+        FRI = "Fri", "Friday"
+        SAT = "Sat", "Saturday"
+        SUN = "Sun", "Sunday"
+
+    books_store = models.ForeignKey(BookStore, on_delete=models.CASCADE)
+    week_day = models.CharField(max_length=5, choices=WeekDay.choices)
+    start_hour = models.PositiveSmallIntegerField(null=True)
+    start_min = models.PositiveSmallIntegerField(null=True)
+    end_hour = models.PositiveSmallIntegerField(null=True)
+    end_min = models.PositiveSmallIntegerField(null=True)
