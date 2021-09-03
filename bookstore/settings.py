e5f0921ec61f949e39398008c2383741211b569b
sam0811956 <sam0811956@gmail.com>
2021-09-03 05:47:38 +0000

    Minor heroku setting

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -134,9 +134,8 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,"bookstore/","staticfiles")
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,"bookstore/","static")
 STATICFILES_DIRS = (
             os.path.join(BASE_DIR,"bookstore/", 'static'),
         )
