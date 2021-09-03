5670773eaab63ddad77cd2384fe045fd39b0acba
sam0811956 <sam0811956@gmail.com>
2021-09-03 12:58:11 +0000

    setting

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -134,12 +134,8 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-            os.path.join(BASE_DIR, 'static'),
-        )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
