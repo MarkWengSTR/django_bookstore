e27784fde1a48a754ab53c73aa9631d9d34febaf
sam0811956 <sam0811956@gmail.com>
2021-09-03 03:36:05 +0000

    setting

--- bookstore/wsgi.py
+++ bookstore/wsgi.py
@@ -10,12 +10,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 from dj_static import Cling
 #sys.path.append('/home/ubuntu/bookstore/django_bookstore/django_bookstore/src/bookstore')
 sys.path.append('/home/ubuntu/bookstore/django_bookstore/django_bookstore/src')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookstore.settings')
 
-application = get_wsgi_application()
-#application = Cling(get_wsgi_application())
+#application = get_wsgi_application()
+application = Cling(get_wsgi_application())
