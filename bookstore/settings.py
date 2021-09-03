f8e5a4434302ba35eed51c557bd54c32967199ae
sam0811956 <sam0811956@gmail.com>
2021-09-03 02:22:38 +0000

    setting

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -135,7 +135,11 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = '/home/ubuntu/bookstore/django_bookstore/django_bookstore/bookstore/static'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
